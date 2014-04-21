25c0bb87c7c51d2c6999579bb9ca8d2993c7e1a5
sWitched-0ff <azhuravlov@gmail.com>
2014-04-21 12:47:11 +0400

    Start

---
+++ js/main.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function(){
+	$('.mainnav a').click(function(event){
+		event.stopPropagation();
+		$(this).siblings().slideToggle();
+	});
+});
+$(document).click(function(){
+	if($(window).width() > 600){
+		$('.mainnav ul ul').slideUp();
+	}
+});
